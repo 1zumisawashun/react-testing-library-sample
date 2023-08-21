de48d869cd87a467c509bffd56fcab2fe3dc1935
1zumisawashun <shun.1zumisawa@gmail.com>
2023-08-21 23:02:55 +0900

    fix

--- src/features/Todo/Todo.test.tsx
+++ src/features/Todo/Todo.test.tsx
@@ -25,8 +25,45 @@ describe("Todo", () => {
     await user.type(inputElement, "Learn Testing Library");
     await user.click(buttonElement);
 
+    // NOTE:special characters
+    // user.type(inputElement, 'Learn Testing Library{enter]');
+
     const listElements = screen.getAllByRole("listitem");
 
     expect(listElements).toHaveLength(4);
+  });
+
+  it("should have style after i add new task and check checkbox", async () => {
+    const user = userEvent.setup();
+
+    render(<Todo />);
+    const inputElement = screen.getByPlaceholderText(/add new task/i);
+    const buttonElement = screen.getByRole("button", { name: "追加" });
+
+    await user.type(inputElement, "Learn Testing Library");
+    await user.click(buttonElement);
+
+    const listElement = screen.getByText(/Learn Testing Library/i);
+
+    await user.click((listElement as any).querySelector("input"));
+
+    expect(listElement).toHaveStyle("text-decoration:line-through");
+  });
+
+  it("should delete task when i click delete X", async () => {
+    const user = userEvent.setup();
+
+    render(<Todo />);
+    const inputElement = screen.getByPlaceholderText(/add new task/i);
+    const buttonElement = screen.getByRole("button", { name: "追加" });
+
+    await user.type(inputElement, "Learn Testing Library");
+    await user.click(buttonElement);
+    expect(screen.getAllByRole("listitem")).toHaveLength(4);
+
+    const listElement = screen.getByText(/Learn Testing Library/i);
+    // NOTE:削除ボタン
+    await user.click((listElement as any).querySelector("span"));
+    expect(screen.getAllByRole("listitem")).toHaveLength(3);
   });
 });
