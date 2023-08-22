d1bbdf2c7d710dd7ef215b8b33fda30a6aed4784
1zumisawashun <shun.1zumisawa@gmail.com>
2023-08-22 23:15:18 +0900

    fix fetch comp

--- src/components/Fetch/Fetch.test.tsx
+++ src/components/Fetch/Fetch.test.tsx
@@ -1,8 +1,7 @@
-import React from "react";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
 import { render, fireEvent, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
+import "@testing-library/jest-dom"; // 多分これいらない
 import Fetch from "./Fetch";
 
 const server = setupServer(
--- src/features/User/UserList.test.tsx
+++ src/features/User/UserList.test.tsx
@@ -1,6 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import UserList from "./UserList";
 
+// NOTE:axiosのmockは必要ない？src/components/Fetch/Fetch.test.tsx
+jest.mock("axios", () => ({
+  __esModule: true,
+  default: {
+    get: () => ({
+      data: [{ name: "Kevin Doe", id: 1 }],
+    }),
+  },
+}));
+
 describe("UserList", () => {
   it("render UserList", () => {
     render(<UserList />);
@@ -13,7 +23,9 @@ describe("UserList", () => {
   it("should render users list", async () => {
     render(<UserList />);
     // axiosでフェッチしているので非同期にコードを書く必要がある
+    // find は async, await 関数と利用できる
     const listElements = await screen.findAllByRole("listitem");
+    // screen.debug();
     expect(listElements[0]).toBeInTheDocument();
   });
 });
